d6828a683ec66a735a086709df3e44dddd7b9cd4
matthew.puzey <matthew.puzey@nhs.net>
2018-05-05 19:58:50 +0100

    add tornado server for my api

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import tornado.ioloop
+import tornado.web
 from api import search
 
-if __name__ == "__main__":
 
-    results = search.movie('blade runner')
-    print(results)
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        results = search.movie('blade runner')
+        print(results)
+        response = {'results': results}
+        self.write(response)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
